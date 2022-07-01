14e99f8ed77d2c94b217dcc544c07c684c27615a
Neha Sahu <nehasahu9424@gmail.com>
2022-07-01 23:06:25 +0530

    isPalindrone

---
+++ Day_44/isPalindrone.cpp
@@ -0,0 +1,8 @@
+   bool isPalindrome(int x) {
+        string s = to_string(x);
+        if(s == string(s.rbegin(), s.rend())) {  
+            return true;
+        } else {
+            return false;
+        } 
+    }
